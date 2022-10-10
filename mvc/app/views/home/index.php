8c95ae7151cb264c9d3929c9b64d5b1753db74b3
rafinurilaf <203040135@mail.unpas.ac.id>
2022-10-10 18:15:23 +0700

    menyelesaikan tugas 3

--- mvc/app/views/home/index.php
+++ mvc/app/views/home/index.php
@@ -1,9 +1,9 @@
 <div class="container">
   <div class="jumbotron mt-5">
     <h1 class="display-4">Selamat Datang</h1>
-    <p class="lead">Halo nama saya <?= $data['nama']; ?></p>
+    <p class="lead">Halo Nama Saya <?= $data['nama']; ?></p>
     <hr class="my-4">
-    <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+    <p>I am an editor, photographer, and student currently living in Karawang. My interests range from photography to design. I am also interested in sports, gaming, and technology.</p>
     <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
   </div>
 </div>
